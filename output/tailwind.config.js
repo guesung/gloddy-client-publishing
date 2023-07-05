e591e5d49e8a853c42050f89cb348e4292ece5a7
guesung <gueit214@naver.com>
2023-07-05 06:31:18 +0000

    refactor : 테스트를 위해 임시로 feature에 action 열어두기

---
+++ output/tailwind.config.js
@@ -0,0 +1,27 @@
+const { colors, pxToRem, animations } = require('./style/theme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors,
+      spacing: pxToRem,
+      fontSize: pxToRem,
+      borderRadius: pxToRem,
+      keyframes: animations,
+      animation: {
+        slideUp: 'slideUp 0.5s ease-in-out',
+      },
+    },
+    fontWeight: {
+      100: '100',
+      300: '300',
+      350: '350',
+      400: '400',
+      700: '700',
+      900: '900',
+    },
+  },
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
