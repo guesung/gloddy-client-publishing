fb060571e54d3b53749aeb803bf0774f36ac5ddf
guesung <gueit214@naver.com>
2023-07-10 12:06:12 +0000

    feat : step1 - zustand로 전역 상태 관리

--- output/app/join/step2/InputForm.tsx
+++ output/app/join/step2/InputForm.tsx
@@ -1,13 +1,18 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 
+import Button from '@/components/common/Button';
 import { Input } from '@/components/common/Input';
+import useJoin from '@/store/useJoin';
 
 type InputType = {
   school: string;
 };
 
 export default function InputForm() {
+  const router = useRouter();
+
   const {
     register,
     formState: { errors },
@@ -15,8 +20,11 @@ export default function InputForm() {
     handleSubmit,
   } = useForm<InputType>();
 
+  const { setJoinValue } = useJoin();
+
   const onSubmit = (data: InputType) => {
-    console.log(data);
+    setJoinValue('school', data.school);
+    router.push('/join/step3');
   };
 
   return (
@@ -27,6 +35,8 @@ export default function InputForm() {
           required: true,
         })}
       />
+
+      <Button text="완료" type="submit" className="absolute bottom-0 w-full" />
     </form>
   );
 }
