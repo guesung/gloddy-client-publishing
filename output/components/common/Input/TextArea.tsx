64ac1dea2832705002a5f4bfe2ff38e8ad5d4e8e
guesung <gueit214@naver.com>
2023-07-22 12:38:39 +0000

    feat : trouble-shooting issue 추가

--- output/components/common/Input/TextArea.tsx
+++ output/components/common/Input/TextArea.tsx
@@ -1,28 +1,27 @@
 'use client';
 
 import cn from '@/utils/cn';
-import { type TextareaHTMLAttributes, forwardRef } from 'react';
+import { type TextareaHTMLAttributes } from 'react';
+import { UseFormRegisterReturn } from 'react-hook-form';
 
 export interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   placeholder: string;
   className?: string;
+  register?: UseFormRegisterReturn;
 }
 
-function TextArea(
-  { placeholder, className, ...props }: TextAreaProps,
-  ref: React.Ref<HTMLTextAreaElement>
-) {
+function TextArea({ placeholder, className, register, ...props }: TextAreaProps) {
   return (
     <textarea
-      ref={ref}
       placeholder={placeholder}
       className={cn(
         'font-500 h-112 w-full resize-none rounded-lg bg-gray5 p-10 px-23 py-14 text-16 text-black outline-none placeholder:text-gray3',
         className
       )}
+      {...register}
       {...props}
     />
   );
 }
 
-export default forwardRef(TextArea);
+export default TextArea;
