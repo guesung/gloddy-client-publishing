92a2b2e4d9ff3fcbb973d38a6d18446861ae57e0
guesung <gueit214@naver.com>
2023-07-23 04:29:23 +0000

    refactor : 더미데이터 분리

--- output/apis/auth/apis.ts
+++ output/apis/auth/apis.ts
@@ -1,12 +1,39 @@
 import publicApi from '../config/publicApi';
 
-import type { LoginResponse } from './type';
+import type { EmailRequest, LoginResponse, SMSRequest, SMSVerifiyRequest } from './type';
 
 const TEST_ID = {
   email: 'testy54@soongsil.ac.kr',
   password: 'qwqw5533',
 };
 
-export const postLogin = () => {
-  return publicApi.post<LoginResponse>('/api/v1/auth/login', TEST_ID);
+export const postLogin = () => publicApi.post<LoginResponse>('/api/v1/auth/login', TEST_ID);
+
+export const postSMS = ({ phoneNumber }: SMSRequest) =>
+  publicApi.post('/api/v1/auth/sms', { number: phoneNumber });
+
+export const postSMSVerify = ({ phoneNumber, verifyCode }: SMSVerifiyRequest) =>
+  publicApi.post('/api/v1/auth/sms/verify-code', {
+    number: phoneNumber,
+    code: verifyCode,
+  });
+
+export const postEmail = ({ email }: EmailRequest) =>
+  publicApi.post('/api/v1/auth/email', { email });
+
+// Test용 Dummy Data
+const obj = {
+  phoneNumber: '010-0000-0000',
+  imageUrl: 'string',
+  schoolInfo: {
+    school: 'string',
+    email: 'string',
+    certifiedStudent: true,
+  },
+  nickname: 'string',
+  birth: '2023-07-22',
+  gender: 'MAIL',
+  personalities: ['OUTGOING'],
 };
+
+export const postSignUp = () => publicApi.post('/api/v1/auth/sign-up', obj);
