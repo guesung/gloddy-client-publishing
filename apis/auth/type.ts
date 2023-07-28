d577b576280d52537f5d52b630299c4eb8660cdd
guesung <gueit214@naver.com>
2023-07-28 02:52:03 +0000

    refactor : gender default 미설정

--- apis/auth/type.ts
+++ apis/auth/type.ts
@@ -1,10 +1,18 @@
 import { GenderType } from '@/types';
 
+export interface LoginRequest {
+  phoneNumber: string;
+}
+
 export interface LoginResponse {
   errorCode: string;
   userId: number;
   authority: string;
-  token: string;
+  token: {
+    accessToken: string;
+    refreshToken: string;
+  };
+  existUser: boolean;
 }
 
 export interface SMSRequest {
@@ -35,7 +43,7 @@ export interface SignUpRequest {
   };
   nickname: string;
   birth: string;
-  gender: GenderType;
+  gender?: GenderType;
   personalities: string[];
 }
 
