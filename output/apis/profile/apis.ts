e61b5b3262e2bfc98822e31990b3ee5ee52d6304
guesung <gueit214@naver.com>
2023-08-04 05:49:24 +0000

    test : ruby & mustache 설치 제거

--- output/apis/profile/apis.ts
+++
@@ -1,5 +0,0 @@
-import privateApi from '../config/privateApi';
-
-import type { ProfileResponse } from './type';
-
-export const getProfile = () => privateApi.get<ProfileResponse>('/me/page');
--- output/app/join/funnels/step4/type.ts
+++
@@ -1,8 +0,0 @@
-import type { DateType, GenderType, ImageType } from '@/types';
-
-export type Step4InputType = {
-  nickname: string;
-  profileImage: ImageType;
-  birthday: DateType;
-  gender: GenderType;
-};
