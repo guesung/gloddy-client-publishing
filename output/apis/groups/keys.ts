57ddc7bba72c4e5cf1a624734fcff2c715314879
guesung <gueit214@naver.com>
2023-08-04 11:01:22 +0000

    test : ruby & mustache 설치 제거

--- output/apis/groups/keys.ts
+++
@@ -1,5 +0,0 @@
-export const Keys = Object.freeze({
-  getGroups: () => ['getGroups'],
-  getGroupDetail: (groupId: number) => ['getGroupDetail', groupId],
-  getArticles: (groupId: number) => ['getArticles', groupId],
-});
