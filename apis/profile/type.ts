fa49fdab0b0560f5c0ce00ed7bc91a937750b4c4
guesung <gueit214@naver.com>
2023-08-07 03:02:43 +0000

    feat : 칭찬 api 구현

--- apis/profile/type.ts
+++ apis/profile/type.ts
@@ -11,3 +11,24 @@ export interface ProfileResponse {
   reviewCount: number;
   school: string;
 }
+
+export interface PraisesResponse {
+  totalCalmCount: number;
+  totalKindCount: number;
+  totalActiveCount: number;
+  totalHumorCount: number;
+  totalAbsenceCount: number;
+  [key: string]: number;
+}
+
+interface Mate {
+  mateImageUrl: string;
+  mateName: string;
+  school: string;
+  createdAt: string; // or Date if you're going to handle this as a Date object
+  selectionReason: string;
+}
+
+export interface MatesResponse {
+  mates: Mate[];
+}
