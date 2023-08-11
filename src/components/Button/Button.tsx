e9984c00c53a51487bf8b0d28f461d4257a4335f
guesung <gueit214@naver.com>
2023-08-11 10:43:36 +0000

    refactor: button 컴포넌트에 outline-warning color 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,25 @@
 import cn from '@/utils/cn';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * 버튼의 크기를 설정합니다. (default: medium)
+   */
   size?: 'small' | 'medium';
-  color?: 'solid-primary' | 'solid-default' | 'solid-warning';
+  /**
+   * 버튼의 색상을 설정합니다. (default: solid-primary)
+   */
+  variant?:
+    | 'solid-primary'
+    | 'solid-default'
+    | 'solid-secondary'
+    | 'outline-warning'
+    | 'solid-warning';
   children: React.ReactNode;
 }
 
 export default function Button({
   size = 'medium',
-  color = 'solid-primary',
+  variant = 'solid-primary',
   className,
   disabled,
   children,
@@ -20,11 +31,15 @@ export default function Button({
         'flex items-center justify-center rounded-8 px-24 py-16 text-subtitle-2',
         {
           'h-56': size === 'medium',
-          'bg-primary text-sign-white disabled:bg-primary-light': color === 'solid-primary',
+          'h-48': size === 'small',
+          'bg-primary text-sign-white disabled:bg-primary-light': variant === 'solid-primary',
           'bg-button text-sign-secondary disabled:bg-sub disabled:text-sign-caption':
-            color === 'solid-default',
+            variant === 'solid-default',
+          'bg-brand-color text-sign-brand disabled:text-sign-white': variant === 'solid-secondary',
+          'border border-warning bg-warning-color text-warning disabled:border-warning-light disabled:bg-white disabled:text-warning-light':
+            variant === 'outline-warning',
           'bg-warning text-sign-white disabled:bg-sub disabled:text-sign-caption':
-            color === 'solid-warning',
+            variant === 'solid-warning',
         },
         className
       )}
