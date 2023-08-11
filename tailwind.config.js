8d1ac23d96e4ae4ad07809e309f92fd8724129d9
guesung <gueit214@naver.com>
2023-08-11 05:12:58 +0000

    refactor : label명 수정 (featuring > feature)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-const { colors, pxToRemTailwind, animations, fontSizes } = require('./src/style/theme');
+const { colors, pxToRemTailwind, animations, fontSizes } = require('./style/theme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       ...pxToRemTailwind,
@@ -11,6 +11,14 @@ module.exports = {
       animation: {
         slideUp: 'slideUp 0.5s ease-in-out',
       },
+    },
+    fontWeight: {
+      100: '100',
+      300: '300',
+      350: '350',
+      400: '400',
+      700: '700',
+      900: '900',
     },
     fontSize: fontSizes,
   },
