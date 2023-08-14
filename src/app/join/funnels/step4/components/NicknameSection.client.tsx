43ce5618b9ed8513a99ce37a014cc1cbf7645f7a
guesung <gueit214@naver.com>
2023-08-14 07:53:39 +0000

    refactor : ref 타입 수정

--- src/app/join/funnels/step4/components/NicknameSection.client.tsx
+++ src/app/join/funnels/step4/components/NicknameSection.client.tsx
@@ -1,28 +1,28 @@
 'use client';
 import { useJoinContext } from '../../../components/JoinContext';
+import { Input } from '@/components/common/Input';
 import { Spacing } from '@/components/common/Spacing';
-import TextFieldController from '@/components/TextField/TextFieldController.client';
 
 export default function NicknameSection() {
-  const hookForm = useJoinContext();
-  const { register } = hookForm;
+  const {
+    register,
+    formState: { errors },
+  } = useJoinContext();
 
   return (
     <section>
       <p className="text-14">닉네임</p>
       <Spacing size={5} />
-      <TextFieldController
+      <Input
         placeholder="닉네임을 입력해주세요."
-        hookForm={hookForm}
         register={register('nickname', {
           required: true,
           pattern: {
-            value: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{3,15}$/,
-            message: '* 최소 3글자 이상 15자 이하로 작성해주세요.',
+            value: /^[a-zA-Z0-9ㄱ-ㅎ가-힣]{3,20}$/,
+            message: '닉네임은 3~20자의 한글, 영문, 숫자만 사용 가능합니다.',
           },
         })}
-        leftCaption="* 최소 3글자 이상 15자 이하로 작성해주세요."
-        maxCount={15}
+        errorMessage={errors.nickname?.message}
       />
     </section>
   );
