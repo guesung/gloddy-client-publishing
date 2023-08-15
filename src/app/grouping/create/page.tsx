6af7f7406f589a080900d1534a2deea0ba2b94bb
guesung <gueit214@naver.com>
2023-08-15 02:16:03 +0000

    refactor : tabs 리팩토링

--- src/app/grouping/create/page.tsx
+++ src/app/grouping/create/page.tsx
@@ -1,12 +1,10 @@
 import CreateGroupContextProvider from './components/CreateGroupContext';
-import GroupingPopup from './components/GroupingPopup.client';
 import InputForm from './components/InputForm.client';
 import { Header } from '@/components/Header';
 
 export default function CreateGroup() {
   return (
     <div className="mx-20">
-      {/* <GroupingPopup /> */}
       <Header />
       <CreateGroupContextProvider>
         <InputForm />
--- src/components/Modal/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ModalWrapper } from './ModalWrapper';
-export { default as BottomSheet } from './BottomSheet';
-export { default as Popup } from './Popup.client';
