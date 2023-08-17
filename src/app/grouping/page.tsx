69c5a1eef9426a0aee40265a54a118c32d4e5ebe
guesung <gueit214@naver.com>
2023-08-17 04:50:44 +0000

    fix : funnel 하위 파일 모두 client component로 변경

--- src/app/grouping/page.tsx
+++ src/app/grouping/page.tsx
@@ -2,25 +2,33 @@ import FloatingBubbleSection from './components/FloatingBubbleSection.server';
 import GroupingCardList from './components/GroupingCardList.client';
 import { getGroups } from '@/apis/groups/apis';
 import { Keys } from '@/apis/groups/keys';
-import { RejectedFallback } from '@/components/common/ErrorBoundary';
+import { RetryErrorBoundary } from '@/components/common/ErrorBoundary';
 import { BottomNavigationBar } from '@/components/common/NavigationBar';
 import { HydrationProvider } from '@/components/common/Provider/HydrationProvider';
 import { Spacing } from '@/components/common/Spacing';
 import { Header } from '@/components/Header';
-import { QueryAsyncBoundary } from '@suspensive/react-query';
+import { Suspense } from 'react';
 
-export default function Grouping() {
+const GroupingComponent = () => {
   return (
-    <>
-      <Header leftNode={<p className="font-700 text-black2">그루핑</p>} />
-      <Spacing size={18} />
-      <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={null}>
+    <RetryErrorBoundary>
+      <Suspense fallback={<div>로딩 중...</div>}>
         <HydrationProvider queryKey={Keys.getGroups()} queryFn={() => getGroups(0)} isInfiniteQuery>
           <GroupingCardList />
         </HydrationProvider>
-      </QueryAsyncBoundary>
+      </Suspense>
+    </RetryErrorBoundary>
+  );
+};
+
+export default function Grouping() {
+  return (
+    <main className="h-full bg-white2">
+      <Header leftNode={<p className="font-700 text-black2">그루핑</p>} />
+      <Spacing size={18} />
+      <GroupingComponent />
       <FloatingBubbleSection />
       <BottomNavigationBar page="grouping" />
-    </>
+    </main>
   );
 }
