124d8dcda901ca0ddd7854dee07d5d3b015e975d
guesung <gueit214@naver.com>
2023-08-18 05:49:04 +0000

    feat : 검색 api 구현

--- src/app/grouping/page.tsx
+++ src/app/grouping/page.tsx
@@ -2,32 +2,33 @@ import FloatingBubbleSection from './components/FloatingBubbleSection.server';
 import GroupingCardList from './components/GroupingCardList.client';
 import { getGroups } from '@/apis/groups/apis';
 import { Keys } from '@/apis/groups/keys';
-import { RejectedFallback } from '@/components/common/ErrorBoundary';
+import { RetryErrorBoundary } from '@/components/common/ErrorBoundary';
 import { BottomNavigationBar } from '@/components/common/NavigationBar';
 import { HydrationProvider } from '@/components/common/Provider/HydrationProvider';
 import { Spacing } from '@/components/common/Spacing';
 import { Header } from '@/components/Header';
-import { QueryAsyncBoundary } from '@suspensive/react-query';
-import Image from 'next/image';
+import { Suspense } from 'react';
 
-export default function Grouping() {
+const GroupingComponent = () => {
   return (
-    <>
-      <Header className="px-20">
-        <Header.Left>매칭</Header.Left>
-        <Header.Right>
-          <Image src="icons/24/search.svg" width={24} height={24} alt="search" />
-        </Header.Right>
-      </Header>
-      <Spacing size={18} />
-
-      <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={null}>
+    <RetryErrorBoundary>
+      <Suspense fallback={<div>로딩 중...</div>}>
         <HydrationProvider queryKey={Keys.getGroups()} queryFn={() => getGroups(0)} isInfiniteQuery>
           <GroupingCardList />
         </HydrationProvider>
-      </QueryAsyncBoundary>
+      </Suspense>
+    </RetryErrorBoundary>
+  );
+};
+
+export default function Grouping() {
+  return (
+    <main className="h-full bg-white2">
+      <Header leftNode={<p className="font-700 text-black2">그루핑</p>} />
+      <Spacing size={18} />
+      <GroupingComponent />
       <FloatingBubbleSection />
       <BottomNavigationBar page="grouping" />
-    </>
+    </main>
   );
 }
