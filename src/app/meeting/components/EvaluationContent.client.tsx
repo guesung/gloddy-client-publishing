596d938b41144511bbcfeca3fba6596dcd227e0e
guesung <gueit214@naver.com>
2023-08-18 04:08:53 +0000

    refactor : Spacing label여부에 따라 on/off

--- src/app/meeting/components/EvaluationContent.client.tsx
+++ src/app/meeting/components/EvaluationContent.client.tsx
@@ -1,21 +1,4 @@
-'use client';
-import { Button } from '@/components/Button';
-import { GroupingCard } from '@/components/Card';
-import { Spacing } from '@/components/common/Spacing';
-import { DUMMY_GROUPING_DATA } from '@/constants/dummyData';
-
 interface EvaluationContentProps {}
 export default function EvaluationContent() {
-  return (
-    <div className="px-20">
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]}>
-        <Spacing size={8} />
-        <Button size="small">모임 평가하기</Button>
-      </GroupingCard>
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]}>
-        <Spacing size={8} />
-        <Button size="small">모임 평가하기</Button>
-      </GroupingCard>
-    </div>
-  );
+  return <div></div>;
 }
--- src/app/meeting/components/ParticipatingContent.client.tsx
+++ src/app/meeting/components/ParticipatingContent.client.tsx
@@ -1,28 +1,4 @@
-'use client';
-import { GroupingCard } from '@/components/Card';
-import { DivisionSpacing, Spacing } from '@/components/common/Spacing';
-import { DUMMY_GROUPING_DATA } from '@/constants/dummyData';
-
 interface ParticipatingContentProps {}
 export default function ParticipatingContent() {
-  return (
-    <div className="px-20">
-      <Spacing size={20} />
-      <div>
-        <Spacing size={4} direction="horizontal" />
-        <span className="text-subtitle-3 text-sign-secondary">멤버로 참여중인 모임</span>
-      </div>
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[0]} />
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[1]} />
-      <Spacing size={8} />
-      <DivisionSpacing size={5} />
-      <Spacing size={20} />
-      <div>
-        <Spacing size={4} direction="horizontal" />
-        <span className="text-subtitle-3 text-sign-secondary">멤버로 참여중인 모임</span>
-      </div>
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[2]} />
-      <GroupingCard groupingData={DUMMY_GROUPING_DATA[1]} />
-    </div>
-  );
+  return <div></div>;
 }
