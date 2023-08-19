81e1c84416c293d7366cf51736058bc78a7a0929
guesung <gueit214@naver.com>
2023-08-19 10:06:41 +0000

    refactor : 넘어갈 경우 email 초기화

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { Flex } from '@/components/Layout';
-import Image from 'next/image';
 import { useEffect } from 'react';
 
 interface ErrorProps {
   error: Error;
+  reset: () => void;
 }
 
 export default function Error({ error }: ErrorProps) {
@@ -14,13 +13,8 @@ export default function Error({ error }: ErrorProps) {
   }, [error]);
 
   return (
-    <Flex align="center" justify="center">
-      <Image src="icons/48/cancel.svg" alt="에러" width={48} height={48} />
-      <p className="text-subtitle-1 text-sign-tertiary">
-        오류가 발생했습니다.
-        <br />
-        잠시 후 다시 시도해 주세요.
-      </p>
-    </Flex>
+    <div>
+      <p>Oh no, something went wrong... maybe refresh?</p>
+    </div>
   );
 }
