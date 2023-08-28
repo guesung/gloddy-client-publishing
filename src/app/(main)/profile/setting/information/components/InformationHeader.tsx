886e381c70dda5a0a6fbf18a5dc8d6fdbc9b8a85
guesung <gueit214@naver.com>
2023-08-28 12:07:24 +0000

    fix : textField의 input 높이 고정

--- src/app/(main)/profile/setting/information/components/InformationHeader.tsx
+++ src/app/(main)/profile/setting/information/components/InformationHeader.tsx
@@ -3,16 +3,16 @@ import { Header } from '@/components/Header';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function EditHeader() {
+export default function InformationHeader() {
   return (
     <Header>
       <Header.Left className="px-4">
         <Link href="/profile/setting">
           <IconButton size="large">
-            <Image src="/icons/24/close.svg" width={24} height={24} alt="back" />
+            <Image src="/icons/24/arrow_back.svg" width={24} height={24} alt="back" />
           </IconButton>
         </Link>
-        <p className="text-subtitle-1">프로필 수정</p>
+        <p className="text-subtitle-1">개인정보 처리 방침</p>
       </Header.Left>
     </Header>
   );
