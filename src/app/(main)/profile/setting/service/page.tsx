e5a4807792f008742783a2bd55f4c2aca0239ef7
guesung <gueit214@naver.com>
2023-09-02 12:22:22 +0000

    refactor : 더미데이터 > 실제 데이터로 교체

--- src/app/(main)/profile/setting/service/page.tsx
+++ src/app/(main)/profile/setting/service/page.tsx
@@ -1,11 +1,9 @@
-import Service from './components/Service';
 import ServiceHeader from './components/ServiceHeader';
 
 export default function page() {
   return (
-    <>
+    <div>
       <ServiceHeader />
-      <Service />
-    </>
+    </div>
   );
 }
