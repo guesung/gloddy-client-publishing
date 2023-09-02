678c983b53c254e703f424c78e0f34e705a2d11d
guesung <gueit214@naver.com>
2023-09-02 14:15:41 +0000

    refactor : 뒤로가기 버튼 router.back()으로 수정

--- src/app/(main)/grouping/components/GroupingCardList.client.tsx
+++ src/app/(main)/grouping/components/GroupingCardList.client.tsx
@@ -2,17 +2,11 @@
 import { useGetGroups } from '@/apis/groups';
 import { GroupingCard } from '@/components/Card';
 import { ItemList } from '@/components/List';
-import useIntersect from '@/hooks/useIntersect';
 
 export default function GroupingCardList() {
-  const { data, fetchNextPage } = useGetGroups();
-
-  const target = useIntersect(fetchNextPage);
+  const { data } = useGetGroups();
 
   return (
-    <>
-      <ItemList data={data} renderItem={(grouping) => <GroupingCard groupingData={grouping} />} />
-      <div ref={target} />
-    </>
+    <ItemList data={data} renderItem={(grouping) => <GroupingCard groupingData={grouping} />} />
   );
 }
