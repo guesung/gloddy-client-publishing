42fef69c1366d2b5a80d2cb0d244b5a546183146
guesung <gueit214@naver.com>
2023-09-03 09:56:14 +0000

    fix: 소개글 잘리는 문제 수정

--- src/apis/profile/keys.ts
+++ src/apis/profile/keys.ts
@@ -1,6 +1,5 @@
 export const Keys = Object.freeze({
   getProfile: () => ['profile'],
-  getProfileById: (userId: number) => ['profile', userId],
   getPraises: () => ['praises'],
   getMates: () => ['mates'],
 });
