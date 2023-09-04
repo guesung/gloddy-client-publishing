b530d0ef09b9c0e99a8dad88b0a1019b395486ce
guesung <gueit214@naver.com>
2023-09-04 08:56:41 +0000

    fix : 들여쓰기 수정

--- src/app/(main)/grouping/page.tsx
+++ src/app/(main)/grouping/page.tsx
@@ -7,14 +7,13 @@ import { RejectedFallback } from '@/components/common/ErrorBoundary';
 import { HydrationProvider } from '@/components/common/Provider';
 import { Spacing } from '@/components/common/Spacing';
 import { Footer } from '@/components/Footer';
-import { Loading } from '@/components/Loading';
 import { QueryAsyncBoundary } from '@suspensive/react-query';
 
 export default function Grouping() {
   return (
     <>
       <GroupingHeader />
-      <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={<Loading />}>
+      <QueryAsyncBoundary rejectedFallback={RejectedFallback} pendingFallback={null}>
         <HydrationProvider queryKey={Keys.getGroups()} queryFn={() => getGroups(0)} isInfiniteQuery>
           <GroupingCardList />
         </HydrationProvider>
--- src/style/theme/animations.ts
+++ src/style/theme/animations.ts
@@ -25,9 +25,4 @@ export const animations = {
     '0%': { transform: 'translateY(0%)' },
     '100%': { transform: 'translateY(100%)' },
   },
-  sizeUpAndDown: {
-    '0%': { transform: 'scale(1)', backgroundColor: '#C7DEFF' },
-    '20%': { transform: 'scale(1.5)', backgroundColor: '#3387FF' },
-    '40%': { transform: 'scale(1)', backgroundColor: '#C7DEFF' },
-  },
 };
