be471285382d7078304dcec10bf1da2b08a61937
guesung <gueit214@naver.com>
2023-09-04 10:00:39 +0000

    refactor: useMoreSheet 훅 추가 및 적용

--- src/apis/common/mutations.ts
+++
@@ -1,6 +0,0 @@
-import { postFiles } from './apis';
-import { useMutation } from '@tanstack/react-query';
-
-export const usePostFiles = () => {
-  return useMutation(postFiles);
-};
--- src/utils/copyToClipboard.ts
+++
@@ -1,7 +0,0 @@
-export async function copyToClipboard(text: string) {
-  if ('clipboard' in navigator) {
-    return await navigator.clipboard.writeText(text);
-  } else {
-    return document.execCommand('copy', true, text);
-  }
-}
