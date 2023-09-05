1ca381975dbc54c0db4b6429b20eee92a7bdbf9b
guesung <gueit214@naver.com>
2023-09-05 01:15:36 +0000

    fix : 아이콘 이름 소문자로 수정

--- src/apis/auth/queries.ts
+++
@@ -1,7 +0,0 @@
-import { Keys, getSchoolSearch } from '.';
-import { useSuspenseQuery } from '@suspensive/react-query';
-
-export const useGetSearchSchool = (searchWord: string) =>
-  useSuspenseQuery(Keys.getSchoolSearch(searchWord), () => getSchoolSearch(searchWord), {
-    enabled: !!searchWord,
-  });
