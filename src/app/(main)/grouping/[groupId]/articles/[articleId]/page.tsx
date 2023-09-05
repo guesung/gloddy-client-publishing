651ca901b69724d3db5fadce6c22b430f3b12aba
guesung <gueit214@naver.com>
2023-09-05 05:48:18 +0000

    refactor: 모달은 close에서 exit로 변경

---
+++ src/app/(main)/grouping/[groupId]/articles/[articleId]/page.tsx
@@ -0,0 +1,40 @@
+import ArticleDetail from './components/ArticleDetail.client';
+import ArticleHeader from './components/ArticleHeader.client';
+import CommentForm from './components/CommentForm';
+import { Keys, getArticle } from '@/apis/groups';
+import { RejectedFallback } from '@/components/common/ErrorBoundary';
+import { HydrationProvider } from '@/components/common/Provider';
+import { Spacing } from '@/components/common/Spacing';
+import { PageAnimation } from '@/components/PageAnimation';
+import { QueryAsyncBoundary } from '@suspensive/react-query';
+
+interface ArticleDetailPageProps {
+  params: {
+    groupId: string;
+    articleId: string;
+  };
+}
+
+export default function ArticlePage({ params }: ArticleDetailPageProps) {
+  const groupId = Number(params.groupId);
+  const articleId = Number(params.articleId);
+
+  return (
+    <>
+      <ArticleHeader />
+      <QueryAsyncBoundary rejectedFallback={RejectedFallback}>
+        <PageAnimation>
+          <HydrationProvider
+            queryFn={() => getArticle(groupId, articleId)}
+            queryKey={Keys.getArticle(groupId, articleId)}
+          >
+            <ArticleDetail />
+          </HydrationProvider>
+        </PageAnimation>
+      </QueryAsyncBoundary>
+      <Spacing size={100} />
+      <CommentForm />
+      <Spacing size={60} />
+    </>
+  );
+}
---
+++ src/utils/date.ts
@@ -0,0 +1,2 @@
+export const currentKoreaTime = new Date(Date.now() + 9 * 60 * 60 * 1000);
+export const oneDay = 1000 * 60 * 60 * 24;
