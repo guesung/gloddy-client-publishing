fdf7049035d1db449d5dbf1ce64260feff2f0bbf
guesung <gueit214@naver.com>
2023-09-05 06:50:25 +0000

    refactor : 토큰 만료 기간 설정

--- .github/ISSUE_TEMPLATE/TroubleShooting.md
+++
@@ -1,9 +0,0 @@
----
-name: Trouble Shooting
-about: 발견한 문제와 문제의 원인, 해결 방안을 제시해주세요.
-title: '[Trouble Shooting] '
-labels: Fix
-assignees: ''
----
-
-## 문제점
--- src/app/(sub)/join/components/JoinContentTextMessage.client.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import type { StrictPropsWithChildren } from '@/types';
-
-export default function JoinContentTextMessage({ children }: StrictPropsWithChildren) {
-  return <p className="text-subtitle-2 text-sign-tertiary">{children}</p>;
-}
