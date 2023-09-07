11fb8c064f50320d188d70b397a58ebf19a4a21b
guesung <gueit214@naver.com>
2023-09-07 01:30:16 +0000

    fix: 모임 개설 일정 선택 시 한달 전으로 보이는 이슈 수정

--- src/components/Modal/Modal.client.tsx
+++ src/components/Modal/Modal.client.tsx
@@ -4,7 +4,6 @@ import { Button } from '../Button';
 import { Spacing } from '../Spacing';
 import { StrictPropsWithChildren } from '@/types';
 import cn from '@/utils/cn';
-import { useEffect } from 'react';
 
 export interface ModalProps {
   onOkClick?: () => void;
@@ -39,22 +38,6 @@ export function Modal({
   className,
   okMessage,
 }: StrictPropsWithChildren<ModalProps>) {
-  let isGoBackClicked = false;
-  useEffect(() => {
-    const goBack = () => {
-      isGoBackClicked = true;
-      onCancelClick && onCancelClick();
-    };
-    history.pushState({ page: 'modal' }, document.title);
-    window.addEventListener('popstate', goBack);
-    return () => {
-      window.removeEventListener('popstate', goBack);
-      if (!isGoBackClicked) {
-        history.back();
-      }
-    };
-  }, [isGoBackClicked]);
-
   return (
     <ModalWrapper onClose={onCancelClick}>
       <div
