a493e7da3a1cde8735d94f247a04c74774037fd7
guesung <gueit214@naver.com>
2023-09-10 04:56:09 +0000

    refactor: isLoading 조건 직관적으로 변경

--- src/apis/profile/queries.ts
+++ src/apis/profile/queries.ts
@@ -5,20 +5,34 @@ import { useSuspenseQuery } from '@suspensive/react-query';
 export const useGetProfile = () =>
   useSuspenseQuery(Keys.getProfile(), getProfile, {
     select: (data) => {
-      const { introduce } = data;
+      const { birth, introduce } = data;
+
+      const formattedBirth = {
+        year: +birth.split('.')[0] + '년',
+        month: +birth.split('.')[1] + '월',
+        date: +birth.split('.')[2] + '일',
+      };
+
       const defaultIntroduce = introduce ?? '';
-      return { ...data, introduce: defaultIntroduce };
+
+      return { ...data, introduce: defaultIntroduce, birth: formattedBirth };
     },
   });
 
 export const useGetProfileById = (userId: number) =>
   useSuspenseQuery(Keys.getProfileById(userId), () => getProfileById(userId), {
     select: (data) => {
-      const { introduce } = data;
+      const { birth, introduce } = data;
+
+      const formattedBirth = {
+        year: +birth.split('.')[0] + '년',
+        month: +birth.split('.')[1] + '월',
+        date: +birth.split('.')[2] + '일',
+      };
 
       const defaultIntroduce = introduce ?? '';
 
-      return { ...data, introduce: defaultIntroduce };
+      return { ...data, introduce: defaultIntroduce, birth: formattedBirth };
     },
   });
 
