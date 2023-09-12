536c2dfb3709a67fd2707db26e6a939177247026
guesung <gueit214@naver.com>
2023-09-12 15:47:20 +0000

    feat: 모임 개설 페이지 다국어 추가

--- src/app/[lng]/(sub)/join/page.tsx
+++ src/app/[lng]/(sub)/join/page.tsx
@@ -1,9 +1,8 @@
 import JoinContextProvider from './components/JoinContext.client';
 import JoinFunnel from './funnels/JoinFunnel';
 import { ModalProvider } from '@/hooks/useModal';
-import { useTranslation } from 'react-i18next';
 
-export default async function JoinPage() {
+export default function JoinPage() {
   return (
     <main className="h-full px-20">
       <JoinContextProvider>
