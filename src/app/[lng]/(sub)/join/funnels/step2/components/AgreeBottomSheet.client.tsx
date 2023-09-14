5f50808b85c8c7bcb599e9a73e6b02e29ca30ae0
guesung <gueit214@naver.com>
2023-09-14 00:47:05 +0000

    translate : 태그 번역

--- src/app/[lng]/(sub)/join/funnels/step2/components/AgreeBottomSheet.client.tsx
+++ src/app/[lng]/(sub)/join/funnels/step2/components/AgreeBottomSheet.client.tsx
@@ -70,7 +70,10 @@ export default function AgreeBottomSheet({ onClose, isOpen }: AgreeBottomSheetPr
           <div
             key={agree.name}
             className="flex h-40 items-center gap-8 bg-sub px-8 text-paragraph-2 text-sign-secondary"
-            onClick={() => handleAgreeCheckList(agree.name)}
+            onClick={() => {
+              console.log(1, agree.name, agree.isAgreed);
+              handleAgreeCheckList(agree.name);
+            }}
           >
             <CircleCheckbox key={agree.name} checked={agree.isAgreed} variant="outline" />
             <p className="">
