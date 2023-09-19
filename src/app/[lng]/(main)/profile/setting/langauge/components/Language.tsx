127f7ec59decd5ae37b994ae03c14afc2b289535
guesung <gueit214@naver.com>
2023-09-19 00:02:15 +0000

    feat : 상호평가 > 버튼 번역

--- src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
+++ src/app/[lng]/(main)/profile/setting/langauge/components/Language.tsx
@@ -13,7 +13,7 @@ export default function Language() {
   const { t } = useTranslation('common');
   const router = useRouter();
 
-  const [language, setLanguage] = useState(getLocalCookie('i18next') || 'en');
+  const [language, setLanguage] = useState(getLocalCookie('i18next') || 'ko');
 
   const handleSubmit = () => {
     setLocalCookie('i18next', language);
