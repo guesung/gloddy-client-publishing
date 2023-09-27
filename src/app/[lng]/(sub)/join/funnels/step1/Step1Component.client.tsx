53e3f46ad7ba59cf7237333c64cddd3d5452bdff
guesung <gueit214@naver.com>
2023-09-27 09:08:10 +0000

    Revert "test : 타입"
    This reverts commit 3e80a20a797851fd2dd0a5fb8d87ca980fe63dfd.

--- src/app/[lng]/(sub)/join/funnels/step1/Step1Component.client.tsx
+++ src/app/[lng]/(sub)/join/funnels/step1/Step1Component.client.tsx
@@ -5,21 +5,12 @@ import NoticeSection from './components/NoticeSection.client';
 import JoinTitleTextMessage from '../../components/JoinTitleTextMessage.client';
 import { useTranslation } from '@/app/i18n/client';
 import { Spacing } from '@/components/Spacing';
-import { AUTH_KEYS } from '@/constants/token';
-import { getTokenFromCookie } from '@/utils/auth/tokenController';
-import { getLocalCookie } from '@/utils/cookieController';
-import { usePathname } from 'next/navigation';
 
 export default function Step1Component() {
   const { t } = useTranslation('join');
-  const pathname = usePathname();
-  const language = getLocalCookie('i18next');
-
   return (
     <main>
       <JoinTitleTextMessage>{t('enterPhoneNumber')}</JoinTitleTextMessage>
-      <JoinTitleTextMessage>{pathname}</JoinTitleTextMessage>
-      <JoinTitleTextMessage>{language}</JoinTitleTextMessage>
       <FormSection />
       <Spacing size={16} />
       <NoticeSection />
