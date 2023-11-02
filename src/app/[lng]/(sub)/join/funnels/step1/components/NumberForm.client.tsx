c307d3cf13d481de7860d33e78e5ee793cf9f5c1
guesung <gueit214@naver.com>
2023-11-02 00:35:06 +0000

    feat : 웹에서 업데이트 알림 구현

--- src/app/[lng]/(sub)/join/funnels/step1/components/NumberForm.client.tsx
+++ src/app/[lng]/(sub)/join/funnels/step1/components/NumberForm.client.tsx
@@ -8,9 +8,9 @@ import { useTimerContext } from '@/components/Provider';
 import { Spacing } from '@/components/Spacing';
 import { TextFieldController } from '@/components/TextField';
 import { regexr } from '@/constants/regexr';
+import useAppRouter from '@/hooks/useAppRouter';
 import { useModal } from '@/hooks/useModal';
 import { setTokenAtCookie } from '@/utils/auth/tokenController';
-import { useRouter } from 'next/navigation';
 import { ElementType, KeyboardEventHandler } from 'react';
 
 import type { SignUpState } from '../../../type';
@@ -30,7 +30,7 @@ export default function NumberForm({ inputStatus, setInputStatus }: NumberSectio
   const { start: timerStart, status: timerStatus } = useTimerContext();
   const { open: openToast } = useModal({ delay: 2000 });
   const { mutate: mutateLogin } = useLoginMutation();
-  const router = useRouter();
+  const { refresh, replace } = useAppRouter();
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement> | React.KeyboardEvent<HTMLInputElement>
@@ -59,22 +59,16 @@ export default function NumberForm({ inputStatus, setInputStatus }: NumberSectio
               refreshToken: response.token.refreshToken,
               userId: response.userId,
             });
-            router.refresh();
-            router.replace('/grouping');
+            refresh();
+            replace('/grouping');
           },
         }
       );
       return;
     }
-    mutateSMS(
-      { number: phoneNumberWithoutHyphen },
-      {
-        onSuccess: () => {
-          openToast(() => <Toast>인증 번호가 전송되었습니다.</Toast>);
-          setInputStatus('afterSend');
-        },
-      }
-    );
+    mutateSMS({ number: phoneNumberWithoutHyphen });
+    openToast(() => <Toast>인증 번호가 전송되었습니다.</Toast>);
+    setInputStatus('afterSend');
   };
 
   return (
