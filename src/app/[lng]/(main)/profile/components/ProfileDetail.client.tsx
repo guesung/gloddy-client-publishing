69ce2e454f5a5409acd95549de2811d7c41f1a32
guesung <gueit214@naver.com>
2023-11-15 15:12:21 +0000

    test

--- src/app/[lng]/(main)/profile/components/ProfileDetail.client.tsx
+++ src/app/[lng]/(main)/profile/components/ProfileDetail.client.tsx
@@ -1,10 +1,26 @@
 'use client';
 
 import ProfileDetailSection from './ProfileDetailSection.client';
+import { postFCMToken } from '@/apis/notifications';
 import { useGetProfile } from '@/apis/profile';
+import { getLocalCookie } from '@/utils/cookieController';
 
 export default function ProfileDetail() {
   const { data: profileData } = useGetProfile();
 
-  return <ProfileDetailSection profileData={profileData} />;
+  return (
+    <button
+      onClick={async () => {
+        const fcmToken = getLocalCookie('fcm');
+        try {
+          const a = await postFCMToken({ token: fcmToken || '' });
+          alert(JSON.stringify(a));
+        } catch (e) {
+          alert(e);
+        }
+      }}
+    >
+      클릭해봐
+    </button>
+  );
 }
