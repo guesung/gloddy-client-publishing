08a2f12daffebd3b34bb9efee8ecea399e2814c6
guesung <gueit214@naver.com>
2023-11-15 13:20:58 +0000

    test

--- src/app/[lng]/(main)/grouping/components/GroupingCardList.client.tsx
+++ src/app/[lng]/(main)/grouping/components/GroupingCardList.client.tsx
@@ -25,16 +25,18 @@ export default function GroupingCardList() {
       .post(
         'https://api.gloddy.shop/api/v1/notifications/tokens',
         {
-          token: fcmToken || '',
+          token:
+            'e8zjE18ZTGmm7i5yL5In0D:APA91bGbR5ojJWMQ9CxXdqOlKUclCj7bVBQdQyYs6vMnKPGWeuAPyrJo7iIvbkUQmP3af4p-OKa1_cvcwxoAKaHsKeYdilOHFirpGSzi1OSRTCaF-011xIej0NeA35vyDLf6Qhe-nAFu',
         },
         {
           headers: {
             USER_ID: 64,
+            'Content-Type': 'application/json',
           },
         }
       )
-      .then(() => {
-        alert(fcmToken);
+      .then((e) => {
+        alert(e);
       })
       .catch((e) => alert(e));
   }, []);
