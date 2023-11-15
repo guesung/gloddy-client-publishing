3ed0b7dc1902145c75c9a27e7224730985c9fd15
guesung <gueit214@naver.com>
2023-11-15 15:10:52 +0000

    test

--- src/app/[lng]/(main)/grouping/components/GroupingCardList.client.tsx
+++ src/app/[lng]/(main)/grouping/components/GroupingCardList.client.tsx
@@ -1,20 +1,12 @@
 'use client';
-import { useGetGroups } from '@/apis/groups';
 import { postFCMToken } from '@/apis/notifications';
-import { GroupingCard } from '@/components/Card';
-import { ItemList } from '@/components/List';
-import { useDidMount } from '@/hooks/common/useDidMount';
-import { useBlockStore } from '@/store/useBlockStore';
 import { getLocalCookie } from '@/utils/cookieController';
-import axios from 'axios';
-import { useEffect, useLayoutEffect } from 'react';
-import { useInView } from 'react-intersection-observer';
 
 export default function GroupingCardList() {
   // const { blockGroupIds } = useBlockStore();
   // const { data, fetchNextPage } = useGetGroups();
 
-  const { ref, inView } = useInView();
+  // const { ref, inView } = useInView();
 
   // useEffect(() => {
   //   if (inView) fetchNextPage();
@@ -49,7 +41,7 @@ export default function GroupingCardList() {
       >
         클릭해봐
       </button>
-      <div ref={ref} />
+      {/* <div ref={ref} /> */}
     </>
   );
 }
