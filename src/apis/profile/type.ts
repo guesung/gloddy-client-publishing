0e40bbf8f78e31701bfe512d2c6faaa5007ab2de
guesung <gueit214@naver.com>
2024-01-16 07:36:16 +0000

    feat: svg아이콘의 fill 속성을 currentColor로 변경한다

--- src/apis/profile/type.ts
+++ src/apis/profile/type.ts
@@ -11,21 +11,25 @@ export interface ProfileResponse {
   school: string;
   introduce: string;
   personalities: Array<PersonalityType['keywordDTO']>;
-  joinAt: string;
+  joinAt: number[];
   reliabilityLevel: ReliabilityType;
   reliabilityScore: 0;
   participatedGroupCount: 0;
   praiseCount: number;
   reviewCount: number;
+  countryName: string;
+  countryImage: string;
 }
 
 export interface ProfileRequest {
   imageUrl: string;
   name: string;
-  birth: string;
   gender: 'MALE' | 'FEMALE';
   introduce: string;
   personalities: Array<PersonalityType['keywordDTO']>;
+  countryName: string;
+  countryImage: string;
+  birth: string;
 }
 
 export interface PraisesResponse {
