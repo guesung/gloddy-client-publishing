5e8e86625acbc11afb119a7564a7686393a7fbbe
guesung <gueit214@naver.com>
2024-01-26 07:04:00 +0000

    fix: 대댓글 입력시 CommentForm에 Focus가 되도록 수정 (#597)

--- src/utils/getIsAndroid.ts
+++ src/utils/getIsAndroid.ts
@@ -3,7 +3,6 @@ import { getIsServer } from './getIsServer';
 export const getIsAndroid = () => {
   const isServer = getIsServer();
   if (isServer) return false;
-  alert(navigator.userAgent);
 
   return navigator.userAgent.match(/Android/i) != null;
 };
