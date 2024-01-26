27ddbbbcd000764459efa95ee368b48bf946d3e3
guesung <gueit214@naver.com>
2024-01-26 09:22:48 +0000

    test

--- src/apis/common/apis.ts
+++ src/apis/common/apis.ts
@@ -6,11 +6,15 @@ import type { FilesRequest, FilesResponse } from '.';
 export const postFiles = async ({ fileList }: FilesRequest) => {
   const formData = new FormData();
 
+  alert('파일 업로드 중입니다. 잠시만 기다려주세요.');
+
   const compressedFileList = await Promise.all(fileList.map((file) => compressImage(file)));
 
+  alert('파일 업로드가 완료되었습니다.');
   compressedFileList.forEach((file) => {
     formData.append('fileList', file);
   });
+  alert('파일 업로드 중입니다. 잠시만 기다려주세요.2222');
 
   return publicApi.post<FilesResponse>('/files', formData, {
     headers: {
