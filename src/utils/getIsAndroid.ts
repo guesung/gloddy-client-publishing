18ee268ff91f5cb051871bffec6a7d54dffbaa1c
guesung <gueit214@naver.com>
2024-01-26 05:28:57 +0000

    test

--- src/utils/getIsAndroid.ts
+++ src/utils/getIsAndroid.ts
@@ -3,6 +3,7 @@ import { getIsServer } from './getIsServer';
 export const getIsAndroid = () => {
   const isServer = getIsServer();
   if (isServer) return false;
+  alert(navigator.userAgent);
 
   return navigator.userAgent.match(/Android/i) != null;
 };
